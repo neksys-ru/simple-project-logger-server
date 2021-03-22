f36e717d4ee99aec2f8eccc3c2542342761c7df1
iolinpic <iolinpic@gmail.com>
2021-03-22 12:45:51 +0300

    disable ssl for project, add docker image support

--- SimpleProjectLoggerServer/Startup.cs
+++ SimpleProjectLoggerServer/Startup.cs
@@ -67,7 +67,7 @@ namespace SimpleProjectLoggerServer
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SimpleProjectLoggerServer v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
